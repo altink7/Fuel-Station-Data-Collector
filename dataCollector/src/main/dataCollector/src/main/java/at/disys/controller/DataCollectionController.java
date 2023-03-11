01a69c4218740aa2be565a09797d41192ea8919d
altink7 <kelmendialtin48@gmail.com>
2023-03-11 12:12:37 +0100

    javadoc added

--- dataCollector/src/main/dataCollector/src/main/java/at/disys/controller/DataCollectionController.java
+++ dataCollector/src/main/dataCollector/src/main/java/at/disys/controller/DataCollectionController.java
@@ -1,4 +1,20 @@
 package at.disys.controller;
 
+import org.springframework.stereotype.Controller;
+
+/***
+ * This class is responsible for the REST API. <br>
+ * There are two API routes: <br>
+ *
+ *  <li>Description:</li> <br>
+ *  <b>/invoices/<customer-id> [POST] </b><br>
+ *  <li>Starts data gathering job</li><br>
+ *  <b>/invoices/<customer-id> [GET]</b> <br>
+ *  <li>Returns invoice PDF with download link and creation time</li>
+ *  <li>Returns 404 Not Found, if it’s not available</li>
+ *
+ */
+
+@Controller
 public class DataCollectionController {
 }
